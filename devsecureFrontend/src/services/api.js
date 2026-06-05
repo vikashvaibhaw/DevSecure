a44ce02e5b3eefcdc302b22e696b88ad18c37eed
vikashvaibhaw <vvaibhaw6@gmail.com>
2026-06-05 23:56:33 +0530

    chore: update API URL to production backend

--- devsecureFrontend/src/services/api.js
+++ devsecureFrontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+// const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://devsecure-1opt.onrender.com/api';
+
 
 const api = axios.create({
   baseURL: API_URL,
